a5add62ddfc9f3645ecf7a59ff7859272f25cba1
김우성 <KimHyangNam09@gmail.com>
2025-07-26 23:30:07 +0900

    feat : Disappear when Text value is changed

---
+++ Assets/02_Script/01_UI/02_Settings/InputFieldFixer.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class InputFieldFixer : MonoBehaviour
+{
+    // Text의 값이 채워져도 Placeholder가 사라지지 않는 문제 해결
+    public TMP_InputField inputID;
+    public TMP_InputField inputPW;
+    
+    public void OnIDValueChanged(string value)
+    {
+        TogglePlaceholder(inputID, value);
+    }
+    
+    public void OnPWValueChanged(string value)
+    {
+        TogglePlaceholder(inputPW, value);
+    }
+
+    private void TogglePlaceholder(TMP_InputField inputField, string text)
+    {
+        if (inputField != null && inputField.placeholder != null)
+        {
+            inputField.placeholder.gameObject.SetActive(string.IsNullOrEmpty(text));
+        }
+    }
+}
