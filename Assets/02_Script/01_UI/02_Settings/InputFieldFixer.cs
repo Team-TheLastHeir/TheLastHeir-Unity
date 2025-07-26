52f1e23ed3433961eec9dcdff82147714c6342f7
김우성 <KimHyangNam09@gmail.com>
2025-07-26 23:48:22 +0900

    feat : add PW

--- Assets/02_Script/01_UI/02_Settings/InputFieldFixer.cs
+++ Assets/02_Script/01_UI/02_Settings/InputFieldFixer.cs
@@ -1,15 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputFieldFixer : MonoBehaviour
 {
     // Text의 값이 채워져도 Placeholder가 사라지지 않는 문제 해결
     public TMP_InputField inputID;
+    public TMP_InputField inputPW;
     
     public void OnIDValueChanged()
     {
-        Debug.Log("inputID.text: " + inputID.text);
         TogglePlaceholder(inputID, inputID.text);
+    }
+
+    public void OnPWValueChanged()
+    {
+        TogglePlaceholder(inputPW, inputPW.text);
     }
 
     private void TogglePlaceholder(TMP_InputField inputField, string text)
