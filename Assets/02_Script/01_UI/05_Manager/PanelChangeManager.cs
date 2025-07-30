52ffb97e1f23574335c3fc8b1c2ff6630ecbc1cd
김우성 <KimHyangNam09@gmail.com>
2025-07-30 15:14:52 +0900

    fix : 파라미터 0개로 변경

--- Assets/02_Script/01_UI/05_Manager/PanelChangeManager.cs
+++ Assets/02_Script/01_UI/05_Manager/PanelChangeManager.cs
@@ -2,24 +2,30 @@ using UnityEngine;
 using System;
 public class PanelChangeManager : MonoBehaviour
 {
-    public void OnPanelChangeClicked(String changePanelName, String currentPanelName)
+    [Header("패널 변경 설정")] 
+    [SerializeField] private GameObject currentPanel;
+    [SerializeField] private GameObject changePanel;
+
+    void Awake()
+    {
+        
+    }
+    public void OnPanelChangeClicked()
     {  
         // 현재 활성화된 패널을 비활성화
-        GameObject currentPanel = GameObject.Find(currentPanelName);
         if (currentPanel != null)
         {
             currentPanel.SetActive(false);
         }
         
         // 변경할 패널을 활성화
-        GameObject changePanel = GameObject.Find(changePanelName);
         if (changePanel != null)
         {
             changePanel.SetActive(true);
         }
         else
         {
-            Debug.LogWarning($"Change panel '{changePanelName}' not found.");
+            Debug.LogWarning($"Change panel '{changePanel}' not found.");
         }
     }
 }
